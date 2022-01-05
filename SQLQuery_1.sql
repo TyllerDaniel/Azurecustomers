012b8a2b2debad8f08c41823945d8afcb6a5385f
gachengomarugu@gmail.com <0725462859>
2022-01-05 11:11:42 +0300

    Create SQLQuery_1.sql

---
+++ SQLQuery_1.sql
@@ -0,0 +1,21 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Albums_Demo](
+	[AlbumId] [int] IDENTITY(1,1) NOT NULL,
+	[AlbumName] [nvarchar](255) NOT NULL,
+	[ReleaseDate] [date] NOT NULL,
+	[ArtistId] [int] NOT NULL,
+	[GenreId] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[AlbumId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Albums]  WITH CHECK ADD  CONSTRAINT [FK_Albums_Artists] FOREIGN KEY([ArtistId])
+REFERENCES [dbo].[Artists] ([ArtistId])
+GO
+ALTER TABLE [dbo].[Albums] CHECK CONSTRAINT [FK_Albums_Artists]
+GO
